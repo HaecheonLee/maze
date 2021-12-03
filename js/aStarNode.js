c2ca1a7df9176db7391a400a1b0c65e50110033f
unknown <dlgocjs1@gmail.com>
2021-12-03 01:48:35 -0500

    aStarNode testing

--- js/aStarNode.js
+++ js/aStarNode.js
@@ -1,21 +1,61 @@
-class aStarNode {
-  constructor() {
+class AStarNode {
+  #_F;
+  #_G;
+  #_H;
+  #_parentNode;
+
+  constructor(G = 0, H = 0, parentNode = null) {
     // F = G + H
     // G is the minimum cost from a starting point to the current node
     // H is the heuristic cost from the current node to an ending point
 
-    this.F = -1;
-    this.G = -1;
-    this.H = -1;
-    this.parentNode = null;
+    this.F = G + H;
+    this.G = G;
+    this.H = H;
+    this.parentNode = parentNode;
   }
 
   compare(node) {
     // return true if the current node has small 'F' than the comparing node
-    if(node instanceof aStarNode) {
+    if(node instanceof AStarNode) {
       return this.F < node.F;
+    } else {
+      throw new Error(`AStartNode.compare(node): ${node} is not instasnce of AStartNode`);
     }
+  }
 
-    return false;
+  get F() {
+    return this.#_F;
+  }
+
+  set F(val) {
+    this.#_F = val;
+  }
+
+  get G() {
+    return this.#_G;
+  }
+
+  set G(val) {
+    this.#_G = val;
+  }
+
+  get H() {
+    return this.#_H;
+  }
+
+  set H(val) {
+    this.#_H = val;
+  }
+
+  get parentNode() {
+    return this.#_parentNode;
+  }
+
+  set parentNode(val) {
+    this.#_parentNode = val;
   }
 }
+
+/* this line is only needed when testing */
+module.exports = AStarNode;
--- js/test/heap.test.js
+++ js/test/heap.test.js
@@ -1,3 +1,4 @@
+const AStarNode = require('../aStarNode');
 const Heap = require('../heap');
 
 
@@ -42,6 +43,35 @@ describe("Heap Testing...", () => {
     for(let i = 0; i < LEN; i++) {
       const least = heap.pop();
       expect(least).toEqual(arr[i]);
+    }
+  });
+
+  it("aStarNode Testing", () => {
+    const heap = new Heap();
+
+    const arr = [];
+    const LEN = 10;
+    for(let i = 0; i < LEN; i++) {
+      for(let j = 0; j < LEN; j++) {
+        // starting to current node:  taxicab distdance
+        // current node to ending:    euclidean distasnce
+
+        const G = i + j;
+        const H = Math.floor(Math.sqrt((LEN - 1 - i)**2 + (LEN - 1 - j)**2));
+
+        const node = new AStarNode(G, H, null);
+        heap.push(node);
+        arr.push(G + H);
+      }
+    }
+
+    arr.sort((a, b) => a - b);
+
+    let pos = 0;
+    while(!heap.empty()) {
+      const minNode = heap.top();
+      heap.pop();
+      expect(minNode.F).toBe(arr[pos++]);
     }
   });
 });
